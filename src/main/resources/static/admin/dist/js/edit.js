0ce136b831c853074c15218ca8fc364817ae75f3
v <3496655347@qq.com>
2021-03-02 18:12:25 +0800

    修复文章无法修改的bug

--- src/main/resources/static/admin/dist/js/edit.js
+++ src/main/resources/static/admin/dist/js/edit.js
@@ -58,27 +58,7 @@ $(function () {
         }
     });
 
-    new AjaxUpload('#uploadCoverImage', {
-        action: '/admin/upload/file',
-        name: 'file',
-        autoSubmit: true,
-        responseType: "json",
-        onSubmit: function (file, extension) {
-            if (!(extension && /^(jpg|jpeg|png|gif)$/.test(extension.toLowerCase()))) {
-                alert('只支持jpg、png、gif格式的文件！');
-                return false;
-            }
-        },
-        onComplete: function (file, r) {
-            if (r != null && r.resultCode == 200) {
-                $("#blogCoverImage").attr("src", r.data);
-                $("#blogCoverImage").attr("style", "width: 128px;height: 128px;display:block;");
-                return false;
-            } else {
-                alert("error");
-            }
-        }
-    });
+
 });
 
 $('#confirmButton').click(function () {
@@ -164,7 +144,6 @@ $('#saveButton').click(function () {
             "blogCategoryId": blogCategoryId,
             "blogTags": blogTags,
             "blogContent": blogContent,
-            "blogCoverImage": blogCoverImage,
             "blogStatus": blogStatus,
             "enableComment": enableComment
         };
